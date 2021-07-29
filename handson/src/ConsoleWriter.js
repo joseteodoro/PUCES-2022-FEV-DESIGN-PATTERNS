bc32a9c37dd60726c58fe4b1a621f42b07d31bf9
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2021-07-29 18:50:58 -0300

    removing handson

--- handson/src/ConsoleWriter.js
+++
@@ -1,7 +0,0 @@
-class FileWriter {
-  consume ({voucher = '', timestamp = new Date()}) {
-    return console.log(JSON.stringify({ voucher, timestamp }));
-  }
-}
-
-module.exports = FileWriter;
--- handson/tests/helpers/index.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const promised = require('chai-as-promised');
-const sinonChai = require('sinon-chai');
-
-chai.use(promised);
-chai.use(sinonChai);
-chai.should();
-global.expect = chai.expect;
-global.assert = chai.assert;
